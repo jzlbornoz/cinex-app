068d63290a8aaca41d948216464af719dc482ab7
jzlbornoz <javieraap10@gmail.com>
2022-05-24 16:53:43 -0400

    Se agrego el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import '../style/components/Footer.css';
 
 const Footer = () => {
     return (
-        <section className="Footer">
-            Footer
+        <section className='Footer'>
+            <div className="Footer-social">
+                <i className="fa-brands fa-instagram fa-2xl" />
+            </div>
+            <div className="Footer-term">
+                <p>Term of use - Privacy Policy</p>
+            </div>
+            <section className="Footer-git">
+                <a href="https://github.com/jzlbornoz">
+                <i className="fa-brands fa-github" />
+                <p>jzlbornoz</p>
+                </a>
+            </section>
         </section>
     )
 }
