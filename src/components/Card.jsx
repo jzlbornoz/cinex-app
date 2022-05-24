436ad6139884ea00b3af2367b0b7d3c1b2dca103
jzlbornoz <javieraap10@gmail.com>
2022-05-24 16:49:20 -0400

    Se creo el grid

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../style/components/Card.css";
+
+const Card = ({film}) => {
+    const Img = "https://image.tmdb.org/t/p/w300" + film.poster_path;
+    return (
+        <section className="Card">
+            <img src={Img} alt={film.title} />
+            <div className='Card-title'><h3>{film.title}</h3></div>
+        </section>
+    )
+}
+
+export { Card }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import { Card } from '../components/Card';
 import "../style/components/Landing.css";
 
 const Landing = () => {
     const { movies } = useContext(AppContext);
     return (
         <section className="Landing">
-            {movies.map(movie => (
-                <h3 key={movie.id}>{movie.title}</h3>
-            ))}
+            <p>Trending</p>
+            <div className="Landing-grid">
+                {movies.map(movie => (
+                    <Card key={movie.id} film={movie} />
+                ))}
+            </div>
         </section>
     )
 }
