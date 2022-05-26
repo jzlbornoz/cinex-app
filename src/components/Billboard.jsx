625e43f6ac5f5b216df492952380f91a8d9b1b77
jzlbornoz <javieraap10@gmail.com>
2022-05-25 22:09:05 -0400

    Se agregaron las series al billboard

--- src/components/Billboard.jsx
+++ src/components/Billboard.jsx
@@ -6,17 +6,25 @@ import '../style/components/Billboard.css'
 
 
 const Billboard = () => {
-    const { movies, toSelect } = useContext(AppContext);
+    const { movies, toSelect, series} = useContext(AppContext);
     const handleSelect = item => () => {
         toSelect(item);
     }
     return (
         <section className='Billboard'>
-            <p>Trending</p>
+            <p>MOVIES</p>
             <div className="Billboard-grid">
                 {movies.map(movie => (
                     <div data-aos="fade-up" key={movie.id} onClick={handleSelect(movie)}>
                         <Card film={movie} />
+                    </div>
+                ))}
+            </div>
+            <p>SERIES</p>
+            <div className="Billboard-grid">
+                {series.map(serie => (
+                    <div data-aos="fade-up" key={serie.id} onClick={handleSelect(serie)}>
+                        <Card film={serie} />
                     </div>
                 ))}
             </div>
