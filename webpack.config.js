4b301d3774d37c559198adbde1a4f89ebbba6be0
jzlbornoz <javieraap10@gmail.com>
2022-05-27 20:28:03 -0400

    Se agrego configuracion al webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+var WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: './src/index.js',
@@ -59,6 +60,28 @@ module.exports = {
       // and not allow any straggling "old" SWs to hang around
       clientsClaim: true,
       skipWaiting: true,
+    }),
+    new WebpackPwaManifest({
+      name: 'Cinex',
+      short_name: 'CNX',
+      description: 'My awesome Progressive Web App!',
+      background_color: '#0a0a0a',
+      crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+      icons: [
+        {
+          src: path.resolve('./public/favicon.ico'),
+          sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+        },
+        {
+          src: path.resolve('public/logo512.webp'),
+          size: '1024x1024' // you can also use the specifications pattern
+        },
+        {
+          src: path.resolve('public/logo512.webp'),
+          size: '1024x1024',
+          purpose: 'maskable'
+        }
+      ]
     }),
 
   ],
