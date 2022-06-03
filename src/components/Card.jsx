d053483d29e613637ac2c9b3ff4202aeafe97c50
jzlbornoz <javieraap10@gmail.com>
2022-06-03 16:04:37 -0400

    Se hizo el build

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../style/components/Card.css";
 import { Link } from 'react-router-dom';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
@@ -7,15 +7,26 @@ const Card = ({ film }) => {
     const placeholder = (path, width) => {
         return path ? `https://image.tmdb.org/t/p/w${width}` + path : "http://www.proedsolutions.com/wp-content/themes/micron/images/placeholders/placeholder_large.jpg"
     }
+    const [heart, setHeart] = useState("Card-favorite");
+    const handleHeart = () => {
+        if (heart == "Card-favorite" ) {
+            setHeart("Card-favorite red");
+        }else{
+            setHeart("Card-favorite");
+        };
+    };
 
     return (
-        <Link to='/review'>
-            <section className="Card" >
+
+        <section className="Card" >
+            <Link to='/review'>
                 <LazyLoadImage src={placeholder(film.poster_path, 300)} />
                 <div className='Card-title'><h3>{film.title ? film.title : film.name}</h3></div>
                 <div className='Card-vote'><p>{film.vote_average}</p></div>
-            </section>
-        </Link>
+            </Link>
+            <div className={heart} onClick={() => handleHeart()}><i className="fa-solid fa-heart" /></div>
+        </section>
+
     )
 }
 
